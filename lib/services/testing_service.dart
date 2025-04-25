b790a2f03b0b8917320e891a84d3603fa495d543
jbrun001 <jbrun001@gold.ac.uk>
2025-04-25 20:15:36 +0100

    final - switch test mode off so nothing in console.

--- lib/services/testing_service.dart
+++ lib/services/testing_service.dart
@@ -1,7 +1,7 @@
 // test logger used to print console information in a standard way
 // allows this information to be turned off when development is complete
 // when testMode is false no testing information is leaked to the console
-bool testMode = true;
+bool testMode = false;
 
 void testLog(String location, String description, Map<String, dynamic> data) {
   if (!testMode) return;
